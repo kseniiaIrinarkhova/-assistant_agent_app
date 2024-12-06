4590c1e21a12d59ab6775d872c76fc3389172814
kseniiaIrinarkhova <kseniiairinarkhova@gmail.com>
2024-12-05 18:57:35 -0800

    invoked TravilySearch

--- app.py
+++ app.py
@@ -24,4 +24,11 @@ with st.form("paper_research", clear_on_submit=True):
     paper_url = st.text_input("Enter the URL of the paper you want to analyze")
     # Get the research question
     submit_button = st.form_submit_button(label='Analyze')
-
+    paper_insights = ""
+    # Check if the form is submitted
+    if submit_button:
+        if paper_url:
+            st.spinner("Analyzing the paper...")
+            # Get the paper insights
+            paper_data = search.invoke(paper_url)
+            print(paper_data)
